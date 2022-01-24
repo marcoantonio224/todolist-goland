ff8f171a78e81975607aa6cd20d1b0004f7ec228
Marco Canchola <marcocanchola2020@gmail.com>
2022-01-23 19:02:56 -0800

    add grpc server

---
+++ grpc/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "todolist/protobuf/todo"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type RegisterTodolistServer struct {
+	pb.UnimplementedTodolistServer
+}
+
+func (s *RegisterTodolistServer) GetFirstItem(ctx context.Context, input *pb.FirstItemRequest) (*pb.FirstItemResponse, error) {
+	log.Printf("Requesting first item with no input")
+	return &pb.FirstItemResponse{Todo: &pb.Todo{Id: 1, Todo: "Learn Go!", Completed: true}}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterTodolistServer(s, &RegisterTodolistServer{})
+	log.Printf("Server listening at : %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
